0dc2a3fb70fa22b4c34d06a018ea976ff4abf109
Jiří Vavřík <jirkavavrik@email.cz>
2018-10-19 14:59:53 +0100

    new license, cell toggling... -license edited to GNU GPL v3 -individual cells can be toggled by clicking on them

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 DOWNLOAD: https://github.com/jirkavavrik/conways-game-of-life/releases/latest
 
-When running this software, the celluar automat "conway's game of life" will run.
+This app simulates J. H. Conway's cellular automaton "Game of life", see https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life for comprehensive article about this.
 
-Cells are generated randomly at start (50% are live, 50% dead).
+Cells are generated randomly at start (50% are live, 50% dead), however you can toggle each cell's state by clicking on it (I recommend doing so when the game is paused - with spacebar or the Pause/Break key).
+The default grid size is 34*45 cells, you can anytime change it by simply resizing the window of the app (this resets the cells to random state again).
 
-You can control speed of simulation with arrow keys.
+You can control speed of simulation with up and down arrow keys.
 
-Program never ends, you have to terminate it by pressing Alt+F4 or the close button.
+Program never ends, you have to terminate it by pressing Q, Alt+F4 or the close button.
+
+You can press F1 anytime for help and info.
--- src/conway.cpp
+++ src/conway.cpp
@@ -1,3 +1,7 @@
+/*
+known bugs:
+-none
+*/
 #include <SDL.h>
 #include <iostream> //cout and cin
 #include <cstdlib> //system(), srand(), rand()
@@ -23,13 +27,15 @@ bool deadCellsAlertShown = false;
 
 SDL_Renderer* renderer;
 
-const char* infoMessage = "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.\n\
-(Wikipedia contributors. \"Conway's Game of Life.\" Wikipedia, The Free Encyclopedia. Wikipedia, The Free Encyclopedia, 21 Sep. 2018. Web. 23 Sep. 2018.)\n\
-	controls:\n\
-	F1: show this info\n\
-	Arrows up and down: change speed of the game\n\
-	Q or Alt+F4 to quit\n\
-	resize app window to change the playing area and/or to reset the game\n\
+const char* infoMessage = "The Game of Life, also known simply as Life, is a cellular automaton \n\
+devised by the British mathematician John Horton Conway in 1970.\n\
+(Wikipedia contributors. \"Conway's Game of Life.\" Wikipedia, The Free Encyclopedia.\n\
+Wikipedia, The Free Encyclopedia, 21 Sep. 2018. Web. 23 Sep. 2018.)\n\
+\tcontrols:\n\
+\tF1: show this info\n\
+\tArrows up and down: change speed of the game\n\
+\tQ or Alt+F4 to quit\n\
+\tresize app window to change the playing area and/or to reset the game\n\
 ";
 
 int main(int argc, char* argv[])
@@ -52,7 +58,7 @@ int previousCountY;
         }
 	
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* MainWindow = SDL_CreateWindow("Conway's game of life by Jiři Vavřík v3.1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowX, windowY, SDL_WINDOW_RESIZABLE); //create window
+	SDL_Window* MainWindow = SDL_CreateWindow("Conway's game of life by Jiři Vavřík v3.2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowX, windowY, SDL_WINDOW_RESIZABLE); //create window
 	renderer = SDL_CreateRenderer(MainWindow,-1,SDL_RENDERER_ACCELERATED);//create renderer
 	
 	bool End = false;
@@ -63,6 +69,7 @@ int previousCountY;
 	drawBackground();
 	while (!End)//main loop
 		{
+
 		
 
 	while (SDL_PollEvent(event)) {//read events from queue
@@ -70,26 +77,20 @@ int previousCountY;
 			if (event->type == SDL_QUIT)
 				End = true;	
 
-			else if (event->type == SDL_KEYDOWN) //if key is pressed
-			{
+			else if (event->type == SDL_KEYDOWN) { //if key is pressed
 				switch (event->key.keysym.sym) //choose the key
 				{
 					case SDLK_UP: //if up arrow is pressed, we decrease delay by 10ms, but protect it from overfowing and getting stuck
-						if(delay > 10)
-							delay -= 10; std::cout << "Delay changed to " << delay << "ms\n"; break;
+								if(delay > 10)
+									delay -= 10; std::cout << "Delay changed to " << delay << "ms\n"; break;
 					case SDLK_DOWN: //if down arrow is pressed, we increase delay (decrease speed)
-						delay += 10; std::cout << "Delay changed to " << delay << "ms\n"; break;
-					case SDLK_F1:
-						SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION,
-                         "Help",
-                         infoMessage,
-                         MainWindow); break;
-					case SDLK_PAUSE: if(Pause) Pause = false; else Pause = true; break;
-					case SDLK_q: End = true; break;
+								delay += 10; std::cout << "Delay changed to " << delay << "ms\n"; break;
+					case SDLK_F1:		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Help", infoMessage, MainWindow); break;
+					case SDLK_PAUSE: 	if(Pause) Pause = false; else Pause = true; break;
+					case SDLK_q:		End = true; break;
 					}
 
-				}
-			else if (event->type == SDL_WINDOWEVENT) {
+			} else if (event->type == SDL_WINDOWEVENT) {
 				switch (event->window.event) {
 					case SDL_WINDOWEVENT_RESIZED:
 						SDL_Log("Window %d resized to %dx%d", event->window.windowID, event->window.data1,	event->window.data2);
@@ -115,7 +116,37 @@ int previousCountY;
 						deadCellsAlertShown = false;
 						break;
 				}
-			}
+			} else if (event->type == SDL_MOUSEBUTTONDOWN) {
+				//routinne for toggling individual cell state
+				int x,y;
+				SDL_GetMouseState(&x, &y);
+				if (event->button.button == SDL_BUTTON_LEFT) {
+    					SDL_Log("Mouse Button 1 (left) is pressed.");std::cout << "mouse x: " << x << " mouse y: " << y << std::endl;
+					if(((x-2) % 14 <= 10 )&&( (y-2) % 14 <= 10 && (y-2)/14 < countY && (x-2)/14 < countX)) {//check if coordinates are on a cell
+						std::cout << "cell clicked\n";
+						std::cout << "cell " << (x-2)/14 << " " << (y-2)/14 << std::endl;
+						//toggle cell state
+						if(currentState[(x-2)/14 + (y-2)/14 * countX] == 1) {
+                    					SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);//white color
+							currentState[(x-2)/14 + (y-2)/14 * countX] = 0;
+                				} else if(currentState[(x-2)/14 + (y-2)/14 * countX] == 0){
+							SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);//blue color
+							currentState[(x-2)/14 + (y-2)/14 * countX] = 1;
+               					}
+						rect->x = ((x-2)/14)*14+2;	
+						rect->y = ((y-2)/14)*14+2;
+						rect->w = 11;
+						rect->h = 11;
+						SDL_RenderFillRect(renderer, rect);//add rectangle to renderer
+						SDL_RenderPresent(renderer); //update screen
+						while(true){//wait until mouse button is released
+							SDL_PollEvent(event);
+							if(event->type == SDL_MOUSEBUTTONUP)
+								break;
+						}
+					}
+				}
+			}//end of cell toggling routinne
 	}//end of reading event queue		
 
 
@@ -127,10 +158,7 @@ int previousCountY;
 	}
 	if(count == 0 && !deadCellsAlertShown) {
 		deadCellsAlertShown = true;
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
-									"Cells died out",
-									"All cells have died out, resizing the app window resets all cells",
-									MainWindow);
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Cells died out", "All cells have died out, resizing the app window resets all cells", MainWindow);
 		}
 
 	if(!Pause){
@@ -182,19 +210,19 @@ char checkLife(short x, short y) {
         surr = currentState[(x+1)+y*countX] + currentState[(x+1)+(y+1)*countX] + currentState[x+(y+1)*countX];
     } else if (x > 0 && y == 0 && x < countX) { //top edge
         surr = currentState[(x-1)+y*countX] + currentState[(x-1)+(y+1)*countX] + currentState[x+(y+1)*countX] + currentState[(x+1)+(y+1)*countX] + currentState[(x+1)+y*countX];
-    } else if(x == countX && y == countY) { //top right corner
+    } else if(x == countX-1 && y == countY-1) { //top right corner
         surr = currentState[(x-1)+y*countX] + currentState[(x-1)+(y+1)*countX] + currentState[x+(y+1)*countX];
-    } else if(y > 0 && x == 0 && y < countY) { //left edge
+    } else if(y > 0 && x == 0 && y < countY-1) { //left edge
         surr = currentState[x+(y-1)*countX] + currentState[(x+1)+(y-1)*countX] + currentState[(x+1)+y*countX] + currentState[(x+1)+(y+1)*countX] + currentState[x+(y+1)*countX];
-    } else if(x > 0 && y > 0 && x < countX && y < countY) { //middle
+    } else if(x > 0 && y > 0 && x < countX-1 && y < countY-1) { //middle
         surr = currentState[(x-1)+(y-1)*countX] + currentState[x+(y-1)*countX] + currentState[(x+1)+(y-1)*countX] + currentState[(x-1)+(y+1)*countX] + currentState[(x+1)+(y+1)*countX] + currentState[x+(y+1)*countX] + currentState[(x+1)+y*countX] + currentState[(x-1)+y*countX];
-    } else if(x == countX && y > 0 && y < countY) { //right edge
+    } else if(x == countX-1 && y > 0 && y < countY-1) { //right edge
         surr = currentState[x+(y+1)*countX] + currentState[(x-1)+(y+1)*countX] + currentState[(x-1)+y*countX] + currentState[(x-1)+(y-1)*countX] + currentState[x+(y-1)*countX];
-    } else if(x == 0 && y == countY) { //lower left corner
+    } else if(x == 0 && y == countY-1) { //lower left corner
         surr = currentState[x+(y-1)*countX] + currentState[(x+1)+(y-1)*countX] + currentState[(x+1)+y*countX];
-    } else if(x > 0 && x < countX && y == countY) { //lower edge
+    } else if(x > 0 && x < countX-1 && y == countY-1) { //lower edge
         surr = currentState[(x-1)+y*countX] + currentState[(x-1)+(y-1)*countX] + currentState[x+(y-1)*countX] + currentState[(x+1)+(y-1)*countX] + currentState[(x+1)+y*countX];
-    } else if(x == countX && y == countY) { //lower right corner
+    } else if(x == countX-1 && y == countY-1) { //lower right corner
         surr = currentState[(x-1)+y*countX] + currentState[(x-1)+(y-1)*countX] + currentState[x+(y-1)*countX];
     }
 	else {
